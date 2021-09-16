fec9412f01d4ea1b1972882265959432d29adc86
Drew Keat <thedrewkeat@gmail.com>
2021-09-16 11:50:38 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let createTaskButton = document.querySelector(
+    "#create-task-form > input[type=submit]:nth-child(3)"
+  );
+  createTaskButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    let newTask = document.getElementById("new-task-description").value;
+    let listItem = document.createElement("li");
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "X";
+    deleteButton.setAttribute('data-description',newTask);
+    listItem.innerText = newTask;
+    listItem.appendChild(deleteButton);
+    let taskList = document.querySelector("#tasks");
+    taskList.appendChild(listItem);
+  });
 });
